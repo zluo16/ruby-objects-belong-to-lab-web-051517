43f475acbaee9e8f2212180be3ebfdcf194e3ee8
zluo16 <zluo@alumni.berklee.edu>
2017-05-17 23:24:58 -0400

    added multiple classes

---
+++ lib/author.rb
@@ -0,0 +1,8 @@
+class Author
+
+  attr_accessor :name
+
+  def initialize
+    @name
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+
+  attr_accessor :title, :artist
+
+  def initialize
+    @title
+    @artist
+  end
+end
